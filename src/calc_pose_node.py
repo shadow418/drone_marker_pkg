0d66caf64245a20015253a3571ce6c89050c2f56
shadow418 <is0323rr@ed.ritsumei.ac.jp>
2018-12-03 16:55:02 +0900

    ちょい変更

--- src/calc_pose_node.py
+++ src/calc_pose_node.py
@@ -10,7 +10,7 @@ from std_msgs.msg import Float32MultiArray
 def callback(msg):
     points = msg.data
     points_2d = np.array([[points[0],points[1]], [points[2],points[3]], [points[4],points[5]], [points[6],points[7]]])
-    #points_2d = np.array([[528.0,140.0], [328.0,140.0], [328.0,340.0], [528.0,340.0]])
+    #points_2d = np.array([[1060.0,340.0], [660.0,340.0], [660.0,740.0], [1060.0,740.0]])
     ret, rvec, tvec = cv2.solvePnP(points_3d, points_2d, camera_mat,dist)
     br.sendTransform((tvec[0],tvec[1],tvec[2]), tf.transformations.quaternion_from_euler(rvec[0], rvec[1], rvec[2]), rospy.Time.now(), "bebop2", "map")
     print tvec
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     br = tf.TransformBroadcaster()
 
     points_3d = np.array([[2.25,2.25,0],[-2.25,2.25,0],[-2.25,-2.25,0],[2.25,-2.25,0]], dtype = np.float32)
-    camera_mat = np.array([[547.883009, 0.000000, 430.632446], [0.000000, 539.585218, 249.283165], [0.000000, 0.000000, 1.000000]])
-    dist = np.array([0.019232, 0.002345, 0.008232, 0.001804, 0.000000])
+    camera_mat = np.array([[1215.7, 0, 952.3442],[0, 1227.4, 542.1231],[0.0, 0.0, 1.0]])
+    dist = np.array([0.0535, -0.1581, 0.3051, 0, 0])
 
     rospy.Subscriber("marker_detect/points", Float32MultiArray, callback)
     rospy.spin()
