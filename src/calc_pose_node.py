f7eb37010cc72b6c46e6844ac78e83b0ce04f0d2
shadow418 <is0323rr@ed.ritsumei.ac.jp>
2018-12-26 14:50:07 +0900

    姿勢計算の改良

--- src/calc_pose_node.py
+++ src/calc_pose_node.py
@@ -12,9 +12,15 @@ def callback(msg):
     points_2d = np.array([[points[0],points[1]], [points[2],points[3]], [points[4],points[5]], [points[6],points[7]], [points[8],points[9]], [points[10],points[11]]])
     #points_2d = np.array([[1060.0,340.0], [660.0,340.0], [660.0,740.0], [1060.0,740.0]])
     ret, rvec, tvec = cv2.solvePnP(points_3d, points_2d, camera_mat,dist)
-    br.sendTransform((-tvec[0],-tvec[1],tvec[2]), tf.transformations.quaternion_from_euler(-rvec[0], rvec[1], rvec[2]), rospy.Time.now(), "bebop2", "map")
-    print tvec
-    print rvec
+
+    R = cv2.Rodrigues(rvec)[0]
+    projMat = np.array([[R[0][0], R[0][1], R[0][2], 0.0], [R[1][0], R[1][1], R[1][2], 0.0], [R[2][0], R[2][1], R[2][2], 1.0]] , dtype = np.float32)
+    eulerAngles = cv2.decomposeProjectionMatrix(projMat)[6]
+    print eulerAngles
+    br.sendTransform((-tvec[0],-tvec[1],tvec[2]), tf.transformations.quaternion_from_euler(eulerAngles[0], 0, eulerAngles[2]), rospy.Time.now(), "bebop2", "map")
+    
+    #print tvec
+    #print rvec
     print "\n"
 
 if __name__ == '__main__':
